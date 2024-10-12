312e7bbf21cd074fcb69801e975e0fc014c4657d
Hazem Al Indari <hazem_jaki@hotmail.com>
2024-10-12 19:56:30 +0200

    Refactor FPGA configuration to support 32-bit width (no support yet, just initial settings)

--- generators/chipyard/src/main/scala/config/RocketFPGAConfigs.scala
+++ generators/chipyard/src/main/scala/config/RocketFPGAConfigs.scala
@@ -9,13 +9,15 @@ import freechips.rocketchip.diplomacy.{AsynchronousCrossing}
 // --------------
 
 class HazemConfig extends Config(
-  new fpga.WithFPGA() ++                              // use FPGA FPGAMMIOChiselModule Module instead of FPGAMMIOBlackBox
-  new freechips.rocketchip.subsystem.WithNBigCores(1) ++         // single rocket-core
-  new freechips.rocketchip.subsystem.WithoutTLMonitors ++         // remove TL monitors for faster simulation
-  new chipyard.config.AbstractConfig)
+    new fpga.WithFPGA(use32Bit = false) ++                              // use SHA3BlackBox 64 bit instead of SHA3 32 bit (default)
+    new freechips.rocketchip.subsystem.WithNBigCores(1) ++         // single rocket-core
+    new freechips.rocketchip.subsystem.WithoutTLMonitors ++         // remove TL monitors for faster simulation
+    new chipyard.config.AbstractConfig
+)
 
-class HazemConfigBlackBox extends Config(
-  new fpga.WithFPGA() ++                              // use FPGA FPGAMMIOBlackBox Module instead of FPGAMMIOChiselModule
-  new freechips.rocketchip.subsystem.WithNBigCores(1) ++         // single rocket-core
-  new freechips.rocketchip.subsystem.WithoutTLMonitors ++         // remove TL monitors for faster simulation
-  new chipyard.config.AbstractConfig)
+class HazemConfig32Bit extends Config(
+    new fpga.WithFPGA(use32Bit = true) ++                              // use SHA3BlackBox 32 bit instead of SHA3 64 bit
+    new freechips.rocketchip.subsystem.WithNBigCores(1) ++         // single rocket-core
+    new freechips.rocketchip.subsystem.WithoutTLMonitors ++         // remove TL monitors for faster simulation
+    new chipyard.config.AbstractConfig
+)
