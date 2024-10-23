4be556c61f0584434b44a7826469dc9fdbeee611
Hazem Al Indari <hazem_jaki@hotmail.com>
2024-10-23 03:24:24 +0200

    256 high 32 working

--- tests/doBuildSHA3.sh
+++ tests/doBuildSHA3.sh
@@ -10,7 +10,11 @@ mkdir -p $BUILD_DIR
 cmake -S ./ -B $BUILD_DIR -D CMAKE_BUILD_TYPE=Debug
 
 # Build the specific testSHA3 targets
-cmake --build $BUILD_DIR --target testSHA3High32Bit
-cmake --build $BUILD_DIR --target testSHA3High64Bit
-cmake --build $BUILD_DIR --target testSHA3Low32Bit
-cmake --build $BUILD_DIR --target testSHA3Low64Bit
+cmake --build $BUILD_DIR --target testSHA3-512High32Bit
+cmake --build $BUILD_DIR --target testSHA3-512High64Bit
+cmake --build $BUILD_DIR --target testSHA3-512Low32Bit
+cmake --build $BUILD_DIR --target testSHA3-512Low64Bit
+cmake --build $BUILD_DIR --target testSHA3-256High32Bit
+cmake --build $BUILD_DIR --target testSHA3-256High64Bit
+#cmake --build $BUILD_DIR --target testSHA3-256Low32Bit
+#cmake --build $BUILD_DIR --target testSHA3-256Low64Bit
---
+++ tests/testSHA3-256High32Bit.c
@@ -0,0 +1,1 @@
+../generators/fpga/c_files/testSHA3-256High32Bit.c
